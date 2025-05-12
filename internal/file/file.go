3fb0823457eb3078617bd63b8effa2c776d92a8b
Alvin Wonys <alvin.rw@proton.me>
2025-05-12 16:00:42 +0800

    refactor: use bufio.Scanner for reading files and wraps error

--- internal/file/file.go
+++ internal/file/file.go
@@ -2,35 +2,28 @@ package file
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 )
 
 func ReadFile(filePath string) ([]string, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("error when opening the file, %v", err)
+		return nil, fmt.Errorf("error when opening the file: %w", err)
 	}
 	defer f.Close()
 
 	fileContent := []string{}
 
-	reader := bufio.NewReader(f)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			} else {
-				return nil, fmt.Errorf("error when reading file, %v", err)
-			}
-		}
-		line = strings.TrimSuffix(line, "\n")
+	scanner := bufio.NewScanner(f)
 
+	for scanner.Scan() {
+		line := scanner.Text()
 		fileContent = append(fileContent, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error when reading file: %w", err)
 	}
 
 	return fileContent, nil
