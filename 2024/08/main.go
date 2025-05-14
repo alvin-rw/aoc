2b564cabbb9b13819a5a25402b9ca45aae3a2d59
Alvin Wonys <alvin.rw@proton.me>
2025-05-14 15:19:32 +0800

    d8p2

--- 2024/08/main.go
+++ 2024/08/main.go
@@ -17,8 +17,10 @@ func main() {
 	antennaCoordinateMap := getAntennaCoordinateMap(mapMatrix)
 
 	numberOfAntinode := calculateNumberOfAntinode(antennaCoordinateMap, maxRow, maxColumn)
-
 	fmt.Printf("number of antinode inside map: %d\n", numberOfAntinode)
+
+	numberOfAntinodeWithResonanceHarmonics := calculateNumberOfAntinodeWithResonanceHarmonics(antennaCoordinateMap, maxRow, maxColumn)
+	fmt.Printf("number of antinode with resonance harmonics: %d\n", numberOfAntinodeWithResonanceHarmonics)
 }
 
 func calculateNumberOfAntinode(antennaCoordinateMap map[string][][]int, maxRow, maxColumn int) int {
@@ -52,6 +54,62 @@ func calculateNumberOfAntinode(antennaCoordinateMap map[string][][]int, maxRow, 
 					addUniqueNumberOfAntinodes(secondAntinodeCoord)
 				}
 			}
+		}
+	}
+
+	return numberOfAntinode
+}
+
+func calculateNumberOfAntinodeWithResonanceHarmonics(antennaCoordinateMap map[string][][]int, maxRow, maxColumn int) int {
+	numberOfAntinode := 0
+
+	antinodesCoordinates := []string{}
+
+	addUniqueNumberOfAntinodes := func(coordinate []int) {
+		c := fmt.Sprintf("%v", coordinate)
+		if !slices.Contains(antinodesCoordinates, c) {
+			antinodesCoordinates = append(antinodesCoordinates, c)
+			numberOfAntinode++
+		}
+	}
+
+	for _, coordinates := range antennaCoordinateMap {
+		for i, firstCoord := range coordinates {
+			for j := i + 1; j < len(coordinates); j++ {
+				secondCoord := coordinates[j]
+
+				rowDist := firstCoord[0] - secondCoord[0]
+				columnDist := firstCoord[1] - secondCoord[1]
+
+				firstCoordError := false
+				secondCoordError := false
+				for k := 1; ; k++ {
+					if !firstCoordError {
+						firstAntinodeCoord := []int{firstCoord[0] + (k * rowDist), firstCoord[1] + (k * columnDist)}
+						if isCoordinateInsideMap(firstAntinodeCoord, maxRow, maxColumn) {
+							addUniqueNumberOfAntinodes(firstAntinodeCoord)
+						} else {
+							firstCoordError = true
+						}
+					}
+
+					if !secondCoordError {
+						secondAntinodeCoord := []int{secondCoord[0] - (k * rowDist), secondCoord[1] - (k * columnDist)}
+						if isCoordinateInsideMap(secondAntinodeCoord, maxRow, maxColumn) {
+							addUniqueNumberOfAntinodes(secondAntinodeCoord)
+						} else {
+							secondCoordError = true
+						}
+					}
+
+					if firstCoordError && secondCoordError {
+						break
+					}
+				}
+
+				addUniqueNumberOfAntinodes(secondCoord)
+			}
+			addUniqueNumberOfAntinodes(firstCoord)
 		}
 	}
 
--- 2024/08/main_test.go
+++ 2024/08/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestCalibrationResult(t *testing.T) {
+func TestCalculateNumberOfAntinode(t *testing.T) {
 	mapMatrix := getMapMatrix("./test.txt")
 
 	maxRow := len(mapMatrix)
@@ -12,6 +12,22 @@ func TestCalibrationResult(t *testing.T) {
 
 	want := 14
 	got := calculateNumberOfAntinode(antennaCoordinateMap, maxRow, maxColumn)
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestCalculateNumberOfAntinodeWithResonanceHarmonics(t *testing.T) {
+	mapMatrix := getMapMatrix("./test.txt")
+
+	maxRow := len(mapMatrix)
+	maxColumn := len(mapMatrix[0])
+
+	antennaCoordinateMap := getAntennaCoordinateMap(mapMatrix)
+
+	want := 34
+	got := calculateNumberOfAntinodeWithResonanceHarmonics(antennaCoordinateMap, maxRow, maxColumn)
 
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
