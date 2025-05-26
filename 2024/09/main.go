731b28d0a29bc76c9d05ca26798c54f4dc4b1931
Alvin Wonys <alvin.rw@proton.me>
2025-05-26 15:19:52 +0800

    d9p2

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -9,13 +10,24 @@ import (
 	"github.com/alvin-rw/aoc/internal/utils"
 )
 
+type diskItem struct {
+	fileId        int
+	startingIndex int
+	size          int
+}
+
 func main() {
 	diskMap := getDiskMap("./input.txt")
 	compactDiskMap(diskMap)
-
 	checksum := calculateChecksum(diskMap)
 
 	fmt.Printf("checksum: %d\n", checksum)
+
+	diskMapv2 := getDiskMapv2("./input.txt")
+	compactDiskMapv2(diskMapv2)
+	checksumv2 := calculateChecksumv2(diskMapv2)
+
+	fmt.Printf("checksum part 2: %d\n", checksumv2)
 }
 
 func calculateChecksum(diskMap []string) int {
@@ -73,3 +85,118 @@ func getDiskMap(inputFilePath string) []string {
 
 	return diskMap
 }
+
+func calculateChecksumv2(diskMap []diskItem) int {
+	checksum := 0
+
+	counter := 0
+
+	for _, di := range diskMap {
+		if di.fileId != -1 {
+			for range di.size {
+				checksum = checksum + (counter * di.fileId)
+				counter++
+			}
+		} else {
+			counter += di.size
+		}
+	}
+
+	return checksum
+}
+
+func compactDiskMapv2(diskMap []diskItem) {
+	for i := len(diskMap) - 1; i >= 0; i-- {
+		if diskMap[i].fileId != -1 {
+			file := diskMap[i]
+
+			for j := range i {
+				if diskMap[j].fileId == -1 && diskMap[j].size >= file.size {
+					file.startingIndex = diskMap[j].startingIndex
+
+					diskMap[i].fileId = -1
+					if diskMap[j].size == file.size {
+						diskMap[j] = file
+					} else if diskMap[j].size > file.size {
+						diskMap[j].startingIndex += file.size
+						diskMap[j].size -= file.size
+						diskMap = slices.Insert(diskMap, j, file)
+					}
+					break
+				}
+			}
+		}
+	}
+}
+
+func getDiskMapv2(inputFilePath string) []diskItem {
+	diskMap := []diskItem{}
+
+	fileContent := file.ReadFile(inputFilePath)
+	diskMapSlice := strings.Split(fileContent[0], "")
+
+	readDiskSize := 0
+
+	for i, n := range diskMapSlice {
+		itemSize, _ := strconv.Atoi(n)
+
+		fileId := -1  // fileId for empty disk space
+		if i%2 == 0 { // file
+			fileId = i / 2
+		}
+
+		di := diskItem{
+			fileId:        fileId,
+			startingIndex: readDiskSize,
+			size:          itemSize,
+		}
+
+		diskMap = append(diskMap, di)
+
+		readDiskSize += itemSize
+	}
+
+	return diskMap
+}
+
+/**
+LOGIC
+
+example disk
+123456
+
+0..111....22222......
+
+diskMap := []diskItem{
+		{
+			fileId:        0,
+			startingIndex: 0,
+			size:    			 1,
+		},
+		{
+			fileId:        -1,
+			startingIndex: 1,
+			size:  			   2,
+		},
+		{
+			fileId:        1,
+			startingIndex: 3,
+			size:  			   3,
+		},
+		{
+			fileId:        -1,
+			startingIndex: 6,
+			size:          4,
+		},
+		{
+			fileId:        2,
+			startingIndex: 10,
+			size:          5,
+		},
+		{
+			fileId:        -1,
+			startingIndex: 15,
+			size:          6,
+		},
+	}
+**/
--- 2024/09/main_test.go
+++ 2024/09/main_test.go
@@ -4,17 +4,14 @@ import "testing"
 
 func TestGetChecksum(t *testing.T) {
 	diskMap := getDiskMap("./test.txt")
-
-	t.Logf("%v\n", diskMap)
-
 	compactDiskMap(diskMap)
+	if got := calculateChecksum(diskMap); got != 1928 {
+		t.Errorf("diskmap v2, got %d, want 1928", got)
+	}
 
-	t.Logf("%v\n", diskMap)
-
-	wantChecksum := 1928
-	checksum := calculateChecksum(diskMap)
-
-	if checksum != wantChecksum {
-		t.Errorf("got %d, want %d", checksum, wantChecksum)
+	diskMapv2 := getDiskMapv2("./test.txt")
+	compactDiskMapv2(diskMapv2)
+	if got := calculateChecksumv2(diskMapv2); got != 2858 {
+		t.Errorf("diskmap v2, got %d, want 2858", got)
 	}
 }
