df9595fb2531d2064ae35550d18853e40eaceed3
Alvin Wonys <alvin.rw@proton.me>
2025-06-09 18:26:56 +0800

    d7p2

--- 2024/07/main.go
+++ 2024/07/main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"slices"
 	"strconv"
 	"strings"
 
 	"github.com/alvin-rw/aoc/internal/file"
-)
-
-const (
-	addition       = "+"
-	multiplication = "*"
 )
 
 func main() {
@@ -21,140 +15,64 @@ func main() {
 		log.Fatalf("error when getting calibration elements: %v", err)
 	}
 
-	totalCalibrationResult := 0
+	partOperator := [][]string{
+		{"+", "*"},
+		{"+", "*", "||"},
+	}
 
-	for _, calibrationElements := range calibrationElementsList {
-		if res, ok := checkCalibrationResult(calibrationElements); ok {
-			totalCalibrationResult += res
+	for i, operators := range partOperator {
+		result := 0
+
+		for _, calibrationElements := range calibrationElementsList {
+			desiredResult := calibrationElements[0]
+			input := calibrationElements[1:]
+
+			if calibrate(input, desiredResult, operators) {
+				result += desiredResult
+			}
 		}
-	}
 
-	fmt.Printf("total calibration result: %d\n", totalCalibrationResult)
+		fmt.Printf("total calibration result part %d: %d\n", i+1, result)
+	}
 }
 
-func checkCalibrationResult(calibrationElements []int) (int, bool) {
-	desiredResult := calibrationElements[0]
-
-	// remove the calibration result from the slice
-	calibrationInputs := slices.Delete(calibrationElements, 0, 1)
-
-	if getHighestPossibleResult(calibrationInputs) < desiredResult {
-		return 0, false
+func calibrate(input []int, desiredResult int, operators []string) bool {
+	if len(input) == 1 {
+		return input[0] == desiredResult
 	}
 
-	if tryCalibration(desiredResult, calibrationInputs) {
-		return desiredResult, true
-	}
-
-	return 0, false
-}
-
-func getHighestPossibleResult(calibrationInputs []int) int {
-	r := calibrationInputs[0]
-
-	for i := 1; i < len(calibrationInputs); i++ {
-		if calibrationInputs[i] > 1 {
-			r = r * calibrationInputs[i]
-		} else {
-			r = r + calibrationInputs[i]
-		}
-	}
-
-	return r
-}
-
-// tryCalibration will try to get the calibration result using all the available
-// operators. Returns true if the calibration result can be obtained from the input
-func tryCalibration(desiredResult int, inputs []int) bool {
-	numberOfOperators := len(inputs) - 1
-
-	for numberOfAdditions := range inputs {
-		additionStartingIndexes := initializeAdditionStartingIndexesSlice(numberOfAdditions)
-
-		for {
-			// make an operators slice and fill it with multiplication
-			operators := make([]string, numberOfOperators)
-			for i := range operators {
-				operators[i] = multiplication
-			}
-
-			addAdditionsToOperatorsSlice(operators, numberOfAdditions, additionStartingIndexes)
-
-			if validateCalibrationOperators(desiredResult, inputs, operators) {
-				return true
-			}
-
-			if ok := incrementAdditionStartingIndex(numberOfOperators-1, numberOfAdditions-1, additionStartingIndexes); !ok {
-				break
-			}
+	for _, o := range operators {
+		calculated := calculateResult(input, o)
+		if calibrate(calculated, desiredResult, operators) {
+			return true
 		}
 	}
 
 	return false
 }
 
-// Initializes the indexes of additions depending on the number of additions.
-//
-// First element will represent the index of the first addition, second element will represent the index
-// of the second addition, etc.
-//
-// Example resulting slice:
-//   - 1 addition: [0] -> +****
-//   - 2 additions: [0 1] -> ++***
-//   - 3 additions: [0 1 2] -> +++**
-func initializeAdditionStartingIndexesSlice(numberOfAdditions int) []int {
-	additionStartingIndexes := make([]int, numberOfAdditions)
-	if numberOfAdditions > 0 {
-		for i := range additionStartingIndexes {
-			additionStartingIndexes[i] = i
-		}
-	}
-
-	return additionStartingIndexes
-}
-
-func addAdditionsToOperatorsSlice(operatorsSlice []string, desiredNumberOfAdditions int, additionStartingIndexes []int) {
-	for i := range desiredNumberOfAdditions {
-		operatorsSlice[additionStartingIndexes[i]] = addition
-	}
-}
-
-// validateCalibrationOperators will calculate the result from the inputs and the operators and
-// compare it with desiredResult. Returns true if the calibration result can be obtained from the provided inputs and operators
-func validateCalibrationOperators(desiredResult int, inputs []int, operators []string) bool {
-	result := inputs[0]
-
-	for i := 1; i < len(inputs); i++ {
-		if operators[i-1] == addition {
-			result = result + inputs[i]
-		} else {
-			result = result * inputs[i]
-		}
-	}
-
-	return result == desiredResult
-}
-
-func incrementAdditionStartingIndex(maxIndex int, indexToIncrement int, additionStartingIndexes []int) bool {
-	if indexToIncrement < 0 {
-		return false
-	}
-
-	additionStartingIndexes[indexToIncrement]++
-
-	if additionStartingIndexes[indexToIncrement] > maxIndex {
-		if indexToIncrement == 0 {
-			return false
+func calculateResult(input []int, operator string) []int {
+	if len(input) >= 2 {
+		r := 0
+		switch operator {
+		case "+":
+			r = input[0] + input[1]
+		case "*":
+			r = input[0] * input[1]
+		case "||":
+			result := fmt.Sprintf("%d%d", input[0], input[1])
+			r, _ = strconv.Atoi(result)
 		}
 
-		if !incrementAdditionStartingIndex(maxIndex-1, indexToIncrement-1, additionStartingIndexes) {
-			return false
+		calculated := []int{r}
+		if len(input) > 2 {
+			calculated = append(calculated, input[2:]...)
 		}
 
-		additionStartingIndexes[indexToIncrement] = additionStartingIndexes[indexToIncrement-1] + 1
+		return calculated
+	} else {
+		return input
 	}
-
-	return true
 }
 
 // return the calibration elements (result and inputs) as slice of []int
--- 2024/07/main_test.go
+++ 2024/07/main_test.go
@@ -1,22 +1,34 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestCalibrationResult(t *testing.T) {
 	cases := []struct {
 		name          string
 		inputFilePath string
+		operators     []string
 		want          int
 	}{
 		{
-			name:          "test file",
+			name:          "test part 1",
 			inputFilePath: "./test.txt",
+			operators:     []string{"+", "*"},
 			want:          3749,
 		},
 		{
-			name:          "input file",
+			name:          "input part 1",
 			inputFilePath: "./input.txt",
+			operators:     []string{"+", "*"},
 			want:          1620690235709,
+		},
+		{
+			name:          "test part 2",
+			inputFilePath: "./test.txt",
+			operators:     []string{"+", "*", "||"},
+			want:          11387,
 		},
 	}
 
@@ -26,16 +38,19 @@ func TestCalibrationResult(t *testing.T) {
 			t.Errorf("an error occured: %v", err)
 		}
 
-		got := 0
+		result := 0
 
 		for _, calibrationElements := range calibrationElementsList {
-			if res, ok := checkCalibrationResult(calibrationElements); ok {
-				got += res
+			desiredResult := calibrationElements[0]
+			input := slices.Delete(calibrationElements, 0, 1)
+
+			if calibrate(input, desiredResult, tt.operators) {
+				result += desiredResult
 			}
 		}
 
-		if got != tt.want {
-			t.Errorf("test name: %s, got %d, want %d", tt.name, got, tt.want)
+		if result != tt.want {
+			t.Errorf("test name: %s, got %d, want %d", tt.name, result, tt.want)
 		}
 	}
 
